e4699e9a851aeec61542710f27f710f94693b17c
Wkana <willian.kana@tecsup.edu.pe>
2024-10-16 15:56:50 -0500

    Initial Commit

---
+++ KanaSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KanaSnapchat
+//
+//  Created by Willian Kana Choquenaira on 16/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
